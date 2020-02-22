02e654c1d7e073ed88c30f39eff86b1bf0670762
miinsuu <myson0545@naver.com>
2020-02-22 21:47:07 +0900

    전화기능구현

--- app/src/main/java/com/momeokji/moc/RestaurantInfoFragment.java
+++ app/src/main/java/com/momeokji/moc/RestaurantInfoFragment.java
@@ -1,10 +1,14 @@
 package com.momeokji.moc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
@@ -59,14 +63,31 @@ public class RestaurantInfoFragment extends Fragment {
         MarqueeTextView preview = view.findViewById(R.id.restaurantInfoPage_previewTxt);
         TextView address = view.findViewById(R.id.restaurantInfoPage_addressTxt);
         TextView phoneNumber = view.findViewById(R.id.restaurantInfoPage_phoneNumberTxt);
+        ImageButton callBtn = view.findViewById(R.id.call_btn);
 
-        //restaurantPage_restaurantName_txt.setText(((MainActivity)getActivity()).restaurantDATA.KoreanRestaurantList.get(0).getRestaurantName());
+        //선택한 가게의 정보를 화면에 뿌려주기
         restaurantPage_restaurantName_txt.setText(selectedRestaurant.getRestaurantName());
         minMaxPrice.setText(selectedRestaurant.getMinMaxPrice());
         preview.setText(selectedRestaurant.getPreview());
         preview.setSelected(true);
         address.setText(selectedRestaurant.getAddress());
         phoneNumber.setText(selectedRestaurant.getPhoneNumber());
+
+        //가게 전화번호로 전화다이얼화면 띄우기
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumStr = selectedRestaurant.getPhoneNumber();
+                if(phoneNumStr.equals(" ") || phoneNumStr.equals(""))
+                    Toast.makeText(getContext(), "이 가게는 전화를 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
+                else {
+                    phoneNumStr =  phoneNumStr.replace("-","");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumStr));
+                    startActivity(intent);
+                }
+            }
+        });
+
 
         final ViewPager restaurantInfoPage_viewPager = view.findViewById(R.id.restaurantInfoPage_viewPager);
         restaurantInfoPage_viewPager.setAdapter(new PagerAdapter_MenuReview(getChildFragmentManager(),1,getActivity()));
