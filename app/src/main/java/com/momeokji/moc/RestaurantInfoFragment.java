9e567b195f0cfc49681c7a244bb4db5421eae43c
miinsuu <myson0545@naver.com>
2020-03-04 13:04:44 +0900

    네이버지도 연동 (앱사용자 현위치표시, 주소기반 가게위치에 이름과 마커표시)

--- app/src/main/java/com/momeokji/moc/RestaurantInfoFragment.java
+++ app/src/main/java/com/momeokji/moc/RestaurantInfoFragment.java
@@ -1,11 +1,17 @@
 package com.momeokji.moc;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,9 +25,20 @@ import com.momeokji.moc.Adapters.PagerAdapter_MenuReview;
 import com.momeokji.moc.CustomView.MarqueeTextView;
 import com.momeokji.moc.data.Restaurant;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 
 public class RestaurantInfoFragment extends Fragment {
     private Restaurant selectedRestaurant;
+    private double lat;
+    private double lng;
 
     public RestaurantInfoFragment(Restaurant selectedRestaurant) {
         this.selectedRestaurant = selectedRestaurant;
@@ -36,9 +53,10 @@ public class RestaurantInfoFragment extends Fragment {
         TextView restaurantPage_restaurantName_txt = view.findViewById(R.id.restaurantInfoPage_restaurantName_txt);
         TextView minMaxPrice = view.findViewById(R.id.restaurantInfoPage_restaurantRangePrice_txt);
         MarqueeTextView preview = view.findViewById(R.id.restaurantInfoPage_previewTxt);
-        TextView address = view.findViewById(R.id.restaurantInfoPage_addressTxt);
+        final TextView address = view.findViewById(R.id.restaurantInfoPage_addressTxt);
         TextView phoneNumber = view.findViewById(R.id.restaurantInfoPage_phoneNumberTxt);
         ImageButton callBtn = view.findViewById(R.id.call_btn);
+        Button addressMapBtn = view.findViewById(R.id.restaurantInfoPage_addressMapBtn);
 
         //선택한 가게의 정보를 화면에 뿌려주기
         restaurantPage_restaurantName_txt.setText(selectedRestaurant.getRestaurantName());
@@ -62,6 +80,31 @@ public class RestaurantInfoFragment extends Fragment {
                 }
             }
         });
+
+        // 가게 주소 -> x,y좌표 변환
+        Geocoder geoCoder = new Geocoder(getContext());
+        try {
+            List<Address> resultLocation =
+                    geoCoder.getFromLocationName(address.getText().toString(), 1);
+            lat = resultLocation.get(0).getLatitude(); // 위도
+            lng = resultLocation.get(0).getLongitude(); // 경도
+            Log.e("주소변환 테스트", "위도: "+lat+", 경도: "+lng);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 가게 주소로 네이버지도 연동 맵 띄우기
+        addressMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MapFragmentActivity.class);
+                intent.putExtra("lat",lat);
+                intent.putExtra("lng",lng);
+                intent.putExtra("name", selectedRestaurant.getRestaurantName());
+                startActivity(intent);
+            }
+        });
+
 
         final ViewPager restaurantInfoPage_viewPager = view.findViewById(R.id.restaurantInfoPage_viewPager);
         restaurantInfoPage_viewPager.setAdapter(new PagerAdapter_MenuReview(getChildFragmentManager(), 1, getActivity()));
