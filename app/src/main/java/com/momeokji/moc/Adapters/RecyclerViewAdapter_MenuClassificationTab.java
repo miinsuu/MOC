73b697c44d5b5e8af04fda132b90bf6c1397eb85
teeraiser1 <teeraiser@naver.com>
2020-03-18 15:29:40 +0900

    MyListPage 메뉴 담기 기능 추가

--- app/src/main/java/com/momeokji/moc/Adapters/RecyclerViewAdapter_MenuClassificationTab.java
+++ app/src/main/java/com/momeokji/moc/Adapters/RecyclerViewAdapter_MenuClassificationTab.java
@@ -3,18 +3,24 @@ package com.momeokji.moc.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.momeokji.moc.MainActivity;
 import com.momeokji.moc.R;
 import com.momeokji.moc.data.Menu;
+import com.momeokji.moc.data.MyListMenu;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter_MenuClassificationTab extends RecyclerView.Adapter<RecyclerViewAdapter_MenuClassificationTab.ItemViewHolder> {
     private ArrayList<Menu> mlist ;
+    private ImageButton myListAddBtn;
 
     public RecyclerViewAdapter_MenuClassificationTab(ArrayList<Menu> mlist) {
         this.mlist = mlist;
@@ -45,11 +51,21 @@ public class RecyclerViewAdapter_MenuClassificationTab extends RecyclerView.Adap
             super(ItemView);
             menuName = ItemView.findViewById(R.id.menuClassificationTab_menuName);
             menuPrice = ItemView.findViewById(R.id.menuClassificationTab_menuPrice);
+            myListAddBtn = ItemView.findViewById(R.id.MyListAddBtn);
         }
 
-        public void onBind(Menu menu){
+        public void onBind(final Menu menu){
             menuName.setText((menu.getName()));
             menuPrice.setText((menu.getPrice()) + "원");
+            myListAddBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (MainActivity.getInstance() != null) {
+                        MainActivity.getInstance().restaurantDATA.MyListMenuList.add(new MyListMenu(menu, MainActivity.getInstance().restaurantDATA.selectedRestaurant.getRestaurantName()));
+                        Toast.makeText(MainActivity.getInstance(), "메뉴가 마이리스트에 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     };
 
